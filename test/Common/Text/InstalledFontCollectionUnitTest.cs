f263f770a868841f3e516250ae3700c22d3b6d4e
dsalvia <damian.salvia@gmail.com>
2024-07-05 00:00:59 -0300

    add test-cases for InstalledFontCollection and PrivateFontCollection

---
+++ test/Common/Text/InstalledFontCollectionUnitTest.cs
@@ -0,0 +1,20 @@
+﻿using GeneXus.Drawing.Text;
+using System;
+using System.IO;
+
+namespace GeneXus.Drawing.Test.Text;
+
+internal class InstalledFontCollectionUnitTest
+{
+	[SetUp]
+	public void Setup()
+	{
+	}
+
+	[Test]
+	public void Constructor_Default()
+	{
+		using var ifc = new InstalledFontCollection();
+		Assert.That(ifc.Families, Is.Not.Empty);
+	}
+}
---
+++ test/Common/Text/PrivateFontCollectionUnitTest.cs
@@ -0,0 +1,60 @@
+﻿using GeneXus.Drawing.Text;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace GeneXus.Drawing.Test.Text;
+
+internal class PrivateFontCollectionUnitTest
+{
+	private static readonly string FONT_PATH = Path.Combine(
+		Directory.GetParent(Environment.CurrentDirectory).Parent.FullName,
+		"res", "fonts");
+
+	[SetUp]
+	public void Setup()
+	{
+	}
+
+	[Test]
+	public void Constructor_Default()
+	{
+		using var pfc = new PrivateFontCollection();
+		Assert.That(pfc.Families, Is.Empty);
+	}
+
+	[Test]
+	public void Method_AddFontFile()
+	{
+		using var pfc = new PrivateFontCollection();
+		string[] fileNames = new[] { "Montserrat-Regular.ttf", "Montserrat-Italic.ttf", "Montserrat-Bold.ttf" };
+		foreach (string fileName in fileNames)
+		{
+			string fontPath = Path.Combine(FONT_PATH, "Montserrat-Regular.ttf");
+			pfc.AddFontFile(fontPath);
+		}
+
+		Assert.That(pfc.Families, Has.Length.EqualTo(3));
+	}
+
+	[Test]
+	public void Method_AddMemoryFont()
+	{
+		using var pfc = new PrivateFontCollection();
+
+		string[] fileNames = new[] { "Montserrat-Regular.ttf", "Montserrat-Italic.ttf", "Montserrat-Bold.ttf" };
+		foreach (string fileName in fileNames)
+		{
+			string fontPath = Path.Combine(FONT_PATH, "Montserrat-Regular.ttf");
+			byte[] fontData = File.ReadAllBytes(fontPath);
+			IntPtr fontPtr = Marshal.AllocCoTaskMem(fontData.Length);
+			Marshal.Copy(fontData, 0, fontPtr, fontData.Length);
+
+			pfc.AddMemoryFont(fontPtr, fontData.Length);
+
+			Marshal.FreeCoTaskMem(fontPtr);
+		}
+
+		Assert.That(pfc.Families, Has.Length.EqualTo(3));
+	}
+}
