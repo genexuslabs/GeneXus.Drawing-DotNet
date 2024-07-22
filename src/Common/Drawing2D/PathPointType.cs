f18809dfbff388ccb82db1df41f1159f027baca4
damiansalvia <damian.salvia@gmail.comm>
2024-07-22 09:34:02 -0300

    define PathPointType as byte

--- src/Common/Drawing2D/PathPointType.cs
+++ src/Common/Drawing2D/PathPointType.cs
@@ -3,42 +3,42 @@ namespace GeneXus.Drawing.Drawing2D;
 /// <summary>
 ///  Specifies the type of point in a <see cref='Drawing.GraphicsPath'/> object.
 /// </summary>
-public enum PathPointType
+public enum PathPointType : byte
 {
 	/// <summary>
 	///  Indicates that the point is the start of a figure.
 	/// </summary>
-	Start = 0,
+	Start = 0x00,
 
 	/// <summary>
 	///  Indicates that the point is an endpoint of a line.
 	/// </summary>
-	Line = 1,
+	Line = 0x01,
 
 	/// <summary>
 	///  Indicates that the point is an endpoint or a control point of a cubic Bézier spline.
 	/// </summary>
-	Bezier = 3,
+	Bezier = 0x03,
 
 	/// <summary>
 	///  Masks all bits except for the three low-order bits, which indicate the point type.
 	/// </summary>
-	PathTypeMask = 7,
+	PathTypeMask = 0x07,
 
 	/// <summary>
 	///  Not used.
 	/// </summary>
-	DashMode = 16,
+	DashMode = 0x10,
 
 	/// <summary>
 	///  Specifies that the point is a marker.
 	/// </summary>
-	PathMarker = 32,
+	PathMarker = 0x20,
 
 	/// <summary>
 	///  Specifies that the point is the last point in a closed subpath (figure).
 	/// </summary>
-	CloseSubpath = 128,
+	CloseSubpath = 0x80,
 
 	/// <summary>
 	///  A cubic Bézier curve.
