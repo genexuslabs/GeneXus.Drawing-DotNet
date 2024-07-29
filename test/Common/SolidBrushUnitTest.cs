710866e5a98519ebb39bdda99dbc59101974d462
damiansalvia <damian.salvia@gmail.comm>
2024-07-29 01:38:59 -0300

    improve SolidBrush unit test

--- test/Common/SolidBrushUnitTest.cs
+++ test/Common/SolidBrushUnitTest.cs
@@ -1,3 +1,5 @@
+using System.IO;
+
 namespace GeneXus.Drawing.Test;
 
 internal class SolidBrushUnitTest
@@ -8,12 +10,22 @@ internal class SolidBrushUnitTest
 	}
 	
 	[Test]
-	public void Constructor_Color()
+	[TestCase("#FF0000", "color-red.png")]
+	[TestCase("#00FF00", "color-green.png")]
+	[TestCase("#0000FF", "color-blue.png")]
+	public void Constructor_Color(string hex, string expected)
 	{
-		var color = Color.Red;
+		var color = new Color(hex);
 
 		using var brush = new SolidBrush(color);
-		Assert.That(brush.Color, Is.EqualTo(color));
+		Assert.Multiple(() =>
+		{
+			Assert.That(brush.Color, Is.EqualTo(color));
+
+			string path = Path.Combine("brush", "solid", expected);
+			float similarity = Utils.CompareImage(path, brush, true);
+			Assert.That(similarity, Is.GreaterThan(0.95));
+		});
 	}
 	
 	[Test]
