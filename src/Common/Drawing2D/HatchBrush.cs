1ab0f613fc9563d9966d125d730ec9d0e9bf7e0c
damiansalvia <damian.salvia@gmail.comm>
2024-08-04 13:45:09 -0300

    simplify HatchBrush.UpdateShader method and improve readability

--- src/Common/Drawing2D/HatchBrush.cs
+++ src/Common/Drawing2D/HatchBrush.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using SkiaSharp;
 
 namespace GeneXus.Drawing.Drawing2D;
@@ -63,293 +64,93 @@ public sealed class HatchBrush : Brush
 
 	#region Utilities
 
+	private static readonly Dictionary<HatchStyle, (int Width, uint[] Pattern)> HATCH_DATA = new()
+	{
+		{ HatchStyle.Horizontal,				(8, new uint[] { 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }) },
+        { HatchStyle.Vertical,					(8, new uint[] { 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80 }) },
+        { HatchStyle.ForwardDiagonal,			(8, new uint[] { 0x80, 0x40, 0x20, 0x10, 0x08, 0x04, 0x02, 0x01 }) },
+        { HatchStyle.BackwardDiagonal,			(8, new uint[] { 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80 }) },
+        { HatchStyle.Cross,						(8, new uint[] { 0xFF, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80 }) },
+        { HatchStyle.DiagonalCross, 			(8, new uint[] { 0x81, 0x42, 0x24, 0x18, 0x18, 0x24, 0x42, 0x81 }) },
+        { HatchStyle.Percent05, 				(8, new uint[] { 0x80, 0x00, 0x00, 0x00, 0x08, 0x00, 0x00, 0x00 }) },
+        { HatchStyle.Percent10, 				(8, new uint[] { 0x80, 0x00, 0x08, 0x00 }) },
+        { HatchStyle.Percent20, 				(4, new uint[] { 0x08, 0x00, 0x02, 0x00 }) },
+        { HatchStyle.Percent25, 				(4, new uint[] { 0x08, 0x02 }) },
+        { HatchStyle.Percent30, 				(4, new uint[] { 0x0A, 0x04, 0x0A, 0x01 }) },
+        { HatchStyle.Percent40, 				(8, new uint[] { 0xAA, 0x55, 0xAA, 0x51, 0xAA, 0x55, 0xAA, 0x15 }) },
+        { HatchStyle.Percent50, 				(2, new uint[] { 0x02, 0x01 }) },
+        { HatchStyle.Percent60, 				(4, new uint[] { 0x0E, 0x05, 0x0B, 0x05 }) },
+        { HatchStyle.Percent70, 				(4, new uint[] { 0x07, 0x0D }) },
+        { HatchStyle.Percent75, 				(4, new uint[] { 0x07, 0x0F, 0x0D, 0x0F }) },
+        { HatchStyle.Percent80, 				(8, new uint[] { 0xEF, 0xFF, 0x7E, 0xFF }) },
+        { HatchStyle.Percent90, 				(8, new uint[] { 0xFF, 0xFF, 0xFF, 0xF7, 0xFF, 0xFF, 0xFF, 0x7F }) },
+        { HatchStyle.LightDownwardDiagonal,		(4, new uint[] { 0x08, 0x04, 0x02, 0x01 }) },
+        { HatchStyle.LightUpwardDiagonal,		(4, new uint[] { 0x01, 0x02, 0x04, 0x08 }) },
+        { HatchStyle.DarkDownwardDiagonal,		(4, new uint[] { 0x0C, 0x06, 0x03, 0x09 }) },
+        { HatchStyle.DarkUpwardDiagonal,		(4, new uint[] { 0x03, 0x06, 0x0C, 0x09 }) },
+        { HatchStyle.WideDownwardDiagonal,		(8, new uint[] { 0xC1, 0xE0, 0x70, 0x38, 0x1C, 0x0E, 0x07, 0x83 }) },
+        { HatchStyle.WideUpwardDiagonal,		(8, new uint[] { 0x83, 0x07, 0x0E, 0x1C, 0x38, 0x70, 0xE0, 0xC1 }) },
+        { HatchStyle.LightVertical,				(4, new uint[] { 0x08, 0x08, 0x08, 0x08 }) },
+        { HatchStyle.LightHorizontal,			(4, new uint[] { 0x0F, 0x00, 0x00, 0x00 }) },
+        { HatchStyle.NarrowVertical,			(2, new uint[] { 0x01, 0x01 }) },
+        { HatchStyle.NarrowHorizontal,			(2, new uint[] { 0x03, 0x00 }) },
+        { HatchStyle.DarkVertical,				(4, new uint[] { 0x0C, 0x0C, 0x0C, 0x0C }) },
+        { HatchStyle.DarkHorizontal,			(4, new uint[] { 0x0F, 0x0F, 0x00, 0x00 }) },
+        { HatchStyle.DashedDownwardDiagonal,	(4, new uint[] { 0x00, 0x00, 0x08, 0x04, 0x02, 0x01, 0x00, 0x00 }) },
+        { HatchStyle.DashedUpwardDiagonal,		(4, new uint[] { 0x00, 0x00, 0x01, 0x02, 0x04, 0x08, 0x00, 0x00 }) },
+        { HatchStyle.DashedHorizontal,			(8, new uint[] { 0xF0, 0x00, 0x00, 0x00, 0x0F, 0x00, 0x00, 0x00 }) },
+        { HatchStyle.DashedVertical,			(8, new uint[] { 0x80, 0x80, 0x80, 0x80, 0x08, 0x08, 0x08, 0x08 }) },
+        { HatchStyle.SmallConfetti,				(8, new uint[] { 0x80, 0x08, 0x40, 0x03, 0x10, 0x01, 0x20, 0x04 }) },
+        { HatchStyle.LargeConfetti,				(8, new uint[] { 0xB1, 0x30, 0x03, 0x1B, 0xD8, 0xC0, 0x06, 0x8D }) },
+        { HatchStyle.ZigZag,					(8, new uint[] { 0x81, 0x42, 0x24, 0x18 }) },
+        { HatchStyle.Wave,						(8, new uint[] { 0x00, 0x18, 0x25, 0xC0 }) },
+        { HatchStyle.DiagonalBrick,				(8, new uint[] { 0x01, 0x02, 0x04, 0x08, 0x18, 0x24, 0x42, 0x81 }) },
+        { HatchStyle.HorizontalBrick,			(8, new uint[] { 0xFF, 0x80, 0x80, 0x80, 0xFF, 0x08, 0x08, 0x08 }) },
+        { HatchStyle.Weave, 					(8, new uint[] { 0x88, 0x54, 0x22, 0x45, 0x88, 0x14, 0x22, 0x51 }) },
+        { HatchStyle.Plaid, 					(8, new uint[] { 0xAA, 0x55, 0xAA, 0x55, 0xF0, 0xF0, 0xF0, 0xF0 }) },
+        { HatchStyle.Divot, 					(8, new uint[] { 0x00, 0x10, 0x08, 0x10, 0x00, 0x80, 0x01, 0x80 }) },
+        { HatchStyle.DottedGrid, 				(8, new uint[] { 0xAA, 0x00, 0x80, 0x00, 0x80, 0x00, 0x80, 0x00 }) },
+        { HatchStyle.DottedDiamond, 			(8, new uint[] { 0x80, 0x00, 0x22, 0x00, 0x08, 0x00, 0x22, 0x00  }) },
+        { HatchStyle.Shingle, 					(8, new uint[] { 0x03, 0x84, 0x48, 0x30, 0x0C, 0x02, 0x01, 0x01 }) },
+        { HatchStyle.Trellis,					(4, new uint[] { 0x0F, 0x06, 0x0F, 0x09 }) },
+        { HatchStyle.Sphere,					(8, new uint[] { 0x77, 0x89, 0x8F, 0x8F, 0x77, 0x98, 0xF8, 0xF8 }) },
+        { HatchStyle.SmallGrid, 				(4, new uint[] { 0x0F, 0x08, 0x08, 0x08 }) },
+        { HatchStyle.SmallCheckerBoard,			(4, new uint[] { 0x09, 0x06, 0x06, 0x09 }) },
+        { HatchStyle.LargeCheckerBoard,			(8, new uint[] { 0xF0, 0xF0, 0xF0, 0xF0, 0x0F, 0x0F, 0x0F, 0x0F }) },
+        { HatchStyle.OutlinedDiamond,			(8, new uint[] { 0x82, 0x44, 0x28, 0x10, 0x28, 0x44, 0x82, 0x01 }) },
+        { HatchStyle.SolidDiamond,				(8, new uint[] { 0x10, 0x38, 0x7C, 0xFE, 0x7C, 0x38, 0x10, 0x00 }) }
+	};
+
 	private void UpdateShader(Action action)
 	{
 		action();
 
-		var size = 10f;
-		using var pattern = new SKBitmap((int)size, (int)size);
+		if (!HATCH_DATA.TryGetValue(m_style, out var data))
+			throw new NotImplementedException($"hatch style {m_style}.");
 
-		using var canvas = new SKCanvas(pattern);
-		canvas.Clear(m_back.m_color);
-
-		var paint = new SKPaint
+		using var bitmap = new SKBitmap(data.Width, data.Pattern.Length);
+		using var canvas = new SKCanvas(bitmap);
+		using var paint = new SKPaint
 		{
-			Color = m_fore.m_color,
-			Style = SKPaintStyle.Stroke,
-			StrokeWidth = 1,
-			IsAntialias = true
+			Color = m_back.m_color,
+			Style = SKPaintStyle.Fill,
+			IsAntialias = false
 		};
 
-		switch (m_style)
+		canvas.Clear(m_fore.m_color);
+
+		for (int x = 0; x < data.Width; x++)
 		{
-			case HatchStyle.Horizontal:
-				canvas.DrawLine(0, size / 2, size, size / 2, paint);
-				break;
-
-			case HatchStyle.Vertical:
-				canvas.DrawLine(size / 2, 0, size / 2, size, paint);
-				break;
-
-			case HatchStyle.ForwardDiagonal:
-				canvas.DrawLine(0, 0, size, size, paint);
-				break;
-
-			case HatchStyle.BackwardDiagonal:
-				canvas.DrawLine(size, 0, 0, size, paint);
-				break;
-
-			case HatchStyle.Cross:
-				canvas.DrawLine(0, size / 2, size, size / 2, paint);
-				canvas.DrawLine(size / 2, 0, size / 2, size, paint);
-				break;
-
-			case HatchStyle.DiagonalCross:
-				canvas.DrawLine(0, 0, size, size, paint);
-				canvas.DrawLine(size, 0, 0, size, paint);
-				break;
-
-			case HatchStyle.DashedDownwardDiagonal:
-				paint.PathEffect = SKPathEffect.CreateDash(new float[] { 4, 2 }, 0);
-				canvas.DrawLine(0, 0, size, size, paint);
-				break;
-
-			case HatchStyle.DashedUpwardDiagonal:
-				paint.PathEffect = SKPathEffect.CreateDash(new float[] { 4, 2 }, 0);
-				canvas.DrawLine(0, size, size, 0, paint);
-				break;
-
-			case HatchStyle.DashedHorizontal:
-				paint.PathEffect = SKPathEffect.CreateDash(new float[] { 4, 2 }, 0);
-				canvas.DrawLine(0, size / 2, size, size / 2, paint);
-				break;
-
-			case HatchStyle.DashedVertical:
-				paint.PathEffect = SKPathEffect.CreateDash(new float[] { 4, 2 }, 0);
-				canvas.DrawLine(size / 2, 0, size / 2, size, paint);
-				break;
-
-			case HatchStyle.SmallConfetti:
-				DrawConfettiPattern(canvas, paint, size, 3);
-				break;
-
-			case HatchStyle.LargeConfetti:
-				DrawConfettiPattern(canvas, paint, size, 5);
-				break;
-
-			case HatchStyle.ZigZag:
-				for (int i = 0; i < size; i += 2)
-				{
-					canvas.DrawLine(i, 0, i + 1, size, paint);
-					canvas.DrawLine(i + 1, size, i + 2, 0, paint);
-				}
-				break;
-
-			case HatchStyle.Wave:
-				for (int i = 0; i < size; i += 2)
-				{
-					canvas.DrawLine(i, size / 2, i + 1, 0, paint);
-					canvas.DrawLine(i + 1, 0, i + 2, size / 2, paint);
-					canvas.DrawLine(i + 2, size / 2, i + 3, size, paint);
-					canvas.DrawLine(i + 3, size, i + 4, size / 2, paint);
-				}
-				break;
-
-			case HatchStyle.Weave:
-				for (int i = 0; i < size; i += 4)
-				{
-					canvas.DrawLine(i, 0, i, size, paint);
-					canvas.DrawLine(0, i, size, i, paint);
-				}
-				break;
-
-			case HatchStyle.Plaid:
-				for (int i = 0; i < size; i += 2)
-				{
-					canvas.DrawLine(i, 0, i, size, paint);
-					canvas.DrawLine(0, i, size, i, paint);
-				}
-				break;
-
-			case HatchStyle.Divot:
-				for (int i = 0; i < size; i += 4)
-				{
-					canvas.DrawLine(i, 0, i + 2, size, paint);
-					canvas.DrawLine(i + 2, 0, i + 4, size, paint);
-				}
-				break;
-
-			case HatchStyle.Shingle:
-				for (int i = 0; i < size; i += 4)
-				{
-					canvas.DrawLine(i, 0, i + 2, size, paint);
-					canvas.DrawLine(i + 2, 0, i + 4, size, paint);
-				}
-				break;
-
-			case HatchStyle.Trellis:
-				for (int i = 0; i < size; i += 2)
-				{
-					canvas.DrawLine(i, 0, i, size, paint);
-					canvas.DrawLine(0, i, size, i, paint);
-				}
-				break;
-
-			case HatchStyle.Sphere:
-				for (int i = 0; i < size; i += 4)
-					for (int j = 0; j < size; j += 4)
-						canvas.DrawCircle(i + 2, j + 2, 2, paint);
-				break;
-
-			case HatchStyle.SmallGrid:
-				for (int i = 0; i < size; i += 2)
-				{
-					canvas.DrawLine(i, 0, i, size, paint);
-					canvas.DrawLine(0, i, size, i, paint);
-				}
-				break;
-
-			case HatchStyle.DottedGrid:
-				for (int i = 0; i < size; i += 2)
-					for (int j = 0; j < size; j += 2)
-						canvas.DrawPoint(i, j, paint);
-				break;
-
-			case HatchStyle.DottedDiamond:
-				for (int i = 0; i < size; i += 2)
-				{
-					canvas.DrawPoint(i, i, paint);
-					canvas.DrawPoint(size - i, i, paint);
-				}
-				break;
-
-			case HatchStyle.SolidDiamond:
-				for (int i = 0; i < size; i += 4)
-				{
-					canvas.DrawLine(i, 0, i + 2, size, paint);
-					canvas.DrawLine(i + 2, 0, i + 4, size, paint);
-				}
-				break;
-
-			case HatchStyle.OutlinedDiamond:
-				for (int i = 0; i < size; i += 4)
-				{
-					canvas.DrawLine(i, 0, i + 2, size, paint);
-					canvas.DrawLine(i + 2, 0, i + 4, size, paint);
-				}
-				break;
-
-			case HatchStyle.DiagonalBrick:
-				for (int i = 0; i < size; i += 4)
-				{
-					canvas.DrawLine(i, 0, i + 2, size, paint);
-					canvas.DrawLine(i + 2, 0, i + 4, size, paint);
-				}
-				break;
-
-			case HatchStyle.HorizontalBrick:
-				for (int i = 0; i < size; i += 4)
-				{
-					canvas.DrawLine(0, i, size, i, paint);
-					canvas.DrawLine(0, i + 2, size, i + 2, paint);
-				}
-				break;
-
-			case HatchStyle.SmallCheckerBoard:
-				DrawCheckerBoardPattern(canvas, paint, m_fore.m_color, m_back.m_color, size, 2);
-				break;
-
-			case HatchStyle.LargeCheckerBoard:
-				DrawCheckerBoardPattern(canvas, paint, m_fore.m_color, m_back.m_color, size, 4);
-				break;
-
-			case HatchStyle.Percent05:
-				DrawPercentagePattern(canvas, paint, size, 5);
-				break;
-
-			case HatchStyle.Percent10:
-				DrawPercentagePattern(canvas, paint, size, 10);
-				break;
-
-			case HatchStyle.Percent20:
-				DrawPercentagePattern(canvas, paint, size, 20);
-				break;
-
-			case HatchStyle.Percent25:
-				DrawPercentagePattern(canvas, paint, size, 25);
-				break;
-
-			case HatchStyle.Percent30:
-				DrawPercentagePattern(canvas, paint, size, 30);
-				break;
-
-			case HatchStyle.Percent40:
-				DrawPercentagePattern(canvas, paint, size, 40);
-				break;
-
-			case HatchStyle.Percent50:
-				DrawPercentagePattern(canvas, paint, size, 50);
-				break;
-
-			case HatchStyle.Percent60:
-				DrawPercentagePattern(canvas, paint, size, 60);
-				break;
-
-			case HatchStyle.Percent70:
-				DrawPercentagePattern(canvas, paint, size, 70);
-				break;
-
-			case HatchStyle.Percent75:
-				DrawPercentagePattern(canvas, paint, size, 75);
-				break;
-
-			case HatchStyle.Percent80:
-				DrawPercentagePattern(canvas, paint, size, 80);
-				break;
-
-			case HatchStyle.Percent90:
-				DrawPercentagePattern(canvas, paint, size, 90);
-				break;
-
-			// TODO: add other styles
-			default:
-				throw new NotSupportedException($"Hatch style {m_style} is not supported.");
-		}
-
-		m_paint.Shader = SKShader.CreateBitmap(pattern, SKShaderTileMode.Repeat, SKShaderTileMode.Repeat);
-	}
-
-	private static void DrawPercentagePattern(SKCanvas canvas, SKPaint paint, float size, int percent)
-	{
-		var rectSize = (int)(size * percent / 100.0);
-		for (int y = 0; y < size; y += rectSize)
-			for (int x = 0; x < size; x += rectSize)
-				canvas.DrawRect(new SKRect(x, y, x + rectSize, y + rectSize), paint);
-	}
-
-	private static void DrawConfettiPattern(SKCanvas canvas, SKPaint paint, float size, int pieces)
-	{
-		var isize = (int)Math.Ceiling(size);
-		var rand = new Random();
-		for (int i = 0; i < pieces; i++)
-		{
-			int x = rand.Next(isize);
-			int y = rand.Next(isize);
-			canvas.DrawPoint(x, y, paint);
-		}
-	}
-
-	private static void DrawCheckerBoardPattern(SKCanvas canvas, SKPaint paint, SKColor fore, SKColor back, float size, int checkerSize)
-	{
-		paint.Color = back;
-		for (int i = 0; i < size; i += checkerSize)
-		{
-			for (int j = 0; j < size; j += checkerSize)
+			for (int y = 0; y < data.Pattern.Length; y++)
 			{
-				paint.Color = paint.Color == fore ? back : fore;
-				canvas.DrawRect(i, j, checkerSize, checkerSize, paint);
+				int offset = data.Width - x;
+				uint mask = 1u << offset - 1;
+				if ((data.Pattern[y] & mask) == mask)
+					canvas.DrawPoint(x, y, paint);
 			}
 		}
+
+		m_paint.Shader = SKShader.CreateBitmap(bitmap, SKShaderTileMode.Repeat, SKShaderTileMode.Repeat);
 	}
 
 
