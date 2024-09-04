cd02f2b2d6882108c9a972fc580660194b149392
damiansalvia <damian.salvia@gmail.comm>
2024-09-04 17:02:49 -0300

    remove unused SKCanvas reference from GraphicsState class

--- src/Common/Drawing2D/GraphicsState.cs
+++ src/Common/Drawing2D/GraphicsState.cs
@@ -6,7 +6,6 @@ namespace GeneXus.Drawing.Drawing2D;
 public sealed class GraphicsState : MarshalByRefObject
 {
     internal int m_index;
-    internal SKCanvas m_canvas;
 
     internal GraphicsState(int state) => m_index = state;
 }
